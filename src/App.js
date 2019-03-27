fcb5b8506149c2ab015ef9b1c34ee5d72a660d66
mdsaon <swesaon@gmail.com>
2019-03-27 21:04:56 +0200

    error handling

--- src/App.js
+++ src/App.js
@@ -6,18 +6,20 @@ import Items from './components/HigherOrderComponentExample/Items';
 import ChartsComponent from './components/ChartsComponent';
 import ButtonTextChange from './components/ButtonTextChange';
 import DataGridExample from './components/DataGridExample';
+import ErrorHandlingWithAxios from './components/ErrorHandlingWithAxios';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <ErrorHandlingWithAxios/>
+         <DataGridExample />
         <ToggleComponent />
         <StyledComponent />
         <HigherOrderComponentExample />
         <Items />
         <ChartsComponent/>
         <ButtonTextChange />
-        <DataGridExample />
       </div>
     );
   }
---
+++ src/components/ErrorHandlingWithAxios/index.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import axios from 'axios';
+
+export default class ErrorHandlingWithAxios extends React.Component {
+  state = {
+    name: '',
+  }
+
+  handleChange = event => {
+    this.setState({ name: event.target.value });
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    const user = {
+      name: this.state.name
+    };
+
+    axios.post(`https://jsonplaceholder.typicode.com/users`, { user })
+      .then(res => {
+        console.log(res);
+        console.log(res.data);
+      })
+      .then(res =>{
+          setTimeout((res) => {
+          document.write('Hello World!');
+          res.end();
+          }, 5000);
+      })
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Person Name:
+            <input type="text" name="name" onChange={this.handleChange} />
+          </label>
+          <button type="submit">Add</button>
+        </form>
+      </div>
+    )
+  }
+}
